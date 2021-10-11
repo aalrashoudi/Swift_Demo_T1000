b07b435615f5a8aacf1d3df61dd0c082680439d0
abdullah <abduulahalrashoudi@Abdullahs-MacBook-Pro.local>
2021-10-11 14:44:03 +0300

    compute salary

---
+++ swift_demo_T1000/Week1/Day1/compute_salary.swift
@@ -0,0 +1,49 @@
+//
+//  compute_salary.swift
+//  swift_demo_T1000
+//
+//  Created by Abdullah AlRashoudi on 10/11/21.
+//
+
+import Foundation
+
+//print("Enter you salary:", terminator: "")
+//let salary = Utils.readInt()
+//let annualSalary = salary * 12
+//print("Annual Salary:", annualSalary)
+//
+//switch salary {
+//case 12000...:
+//    print("High Salary")
+//case 6000..<12000:
+//    print("Normal Salary")
+//case 3000..<6000:
+//    print("Low Salary")
+//    
+//default:
+//    print("Very Low Salary")
+//    
+//}
+
+/*
+ Use Switch
+ switch pct {
+ case 85...:
+     print("excellent")
+
+ case 75..<85:
+     print ("very good")
+
+
+ case 65..<75:
+    print ("good")
+
+ case 50..<65:
+    print ("pass")
+
+ default:
+
+     print("fail")
+ }
+
+ */
--- swift_demo_T1000/main.swift
+++ swift_demo_T1000/main.swift
@@ -7,19 +7,33 @@
 
 import Foundation
 
-//let x = Int.random(in: 0..<35)
-//
-//let y = Int.random(in: 0..<45)
-//
-//print("What's \(x) * \(y) ? ", terminator: "")
-//
-//let answer = Utils.readInt()
-//
-////if answer == x*y {
-////    print("Your Answer Is Correct")
-////} else {
-////    print("Your Answer Is Wrong")
-////}
-////
-////
-//print(answer == x*y ? "Correct" : "Wrong")
+print("Enter your mark")
+let mark = Utils.readDouble()
+print("Enter your Full mark")
+let fullmark = Utils.readInt()
+
+
+
+let pct = mark / Double(fullmark) * 100
+print("The persentage:", round(pct * 100)/100, "%")
+
+
+switch pct {
+case 85...:
+    print("excellent")
+
+case 75..<85:
+    print ("very good")
+
+
+case 65..<75:
+   print ("good")
+
+case 50..<65:
+   print ("pass")
+
+default:
+
+    print("fail")
+}
+
